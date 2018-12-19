0bec0ab71cdeaee215ccab205d73977547ce0bda
Patrick Peng <patrick@Patricks-MacBook-Pro.local>
2018-12-18 20:52:02 -0600

    Q136

--- BitManipulation/136.Single_Number/136.Single_Number_BitManipulation.py
+++ BitManipulation/136.Single_Number/136.Single_Number_BitManipulation.py
@@ -1,6 +1,8 @@
 class Solution(object):
 	def singleNumberBitManipulation(self, nums):
 		"""
+		Time - O(n)
+		Space - O(1)
 		:type nums: List[int]
 		:rtype: int
 		"""
@@ -11,8 +13,11 @@ class Solution(object):
 
 	def singelNumberBitManipulation2(self, nums):
 		"""
+		Time - O(n)
+		Space - O(1)
 		:type nums: List[int]
 		:rtype: int
 		"""
 		from functools import reduce
 		return reduce(lambda i, j: int(i) ^ int(j), nums)
+
---
+++ BitManipulation/136.Single_Number/136.Single_Number_ListOperation.py
@@ -0,0 +1,16 @@
+class Solution(object):
+        def singelNumberList(self, nums):
+                """
+                Time - O(n^2)
+                Space - O(n)
+                :type nums: List[int]
+                :rtype: int
+                """
+                no_duplicate_list = []
+                for i in nums:
+                        if i not in no_duplicate_list:
+                                no_duplicate_list.append(i)
+                        else:
+                                no_duplicate_list.remove(i)
+                return no_duplicate_list.pop()
+
