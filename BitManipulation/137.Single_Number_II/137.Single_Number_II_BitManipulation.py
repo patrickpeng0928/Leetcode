210bd548382891e8f2a7e4c5e4b1f9d7eeee2a3a
Patrick Peng <patrick@Patricks-MacBook-Pro.local>
2018-12-18 23:41:49 -0600

    Q137

--- BitManipulation/137.Single_Number_II/137.Single_Number_II_BitManipulation.py
+++ BitManipulation/137.Single_Number_II/137.Single_Number_II_BitManipulation.py
@@ -1,23 +1,32 @@
 class Solution(object):
-	def singleNumberBitManipulation(self, nums):
+	def singleNumberIIBitManipulation(self, nums):
 		"""
 		Time - O(n)
 		Space - O(1)
 		:type nums: List[int]
 		:rtype: int
 		"""
-		a = 0
-		for i in nums:
-			a ^= i
-		return a
+		one, two, three = 0, 0, 0
+		for num in nums:
+			two |= one & num
+			one ^= num
+			three = one & two
+			one &= ~three
+			two &= ~three
+		return one
 
-	def singelNumberBitManipulation2(self, nums):
+	def singleNumberIIBitManipulation2(self, nums):
 		"""
 		Time - O(n)
 		Space - O(1)
 		:type nums: List[int]
 		:rtype: int
 		"""
-		from functools import reduce
-		return reduce(lambda i, j: int(i) ^ int(j), nums)
-
+		one, two, three = 0, 0, 0
+		for num in nums:
+			three = two & num
+			two |= one & num
+			one ^= num
+			one &= ~three
+			two &= ~three
+		return one
