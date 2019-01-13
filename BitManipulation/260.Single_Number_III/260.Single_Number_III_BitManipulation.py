0e367959c3cfa67f5fb0996a9513cb0e27646ba5
Patrick Peng <patrick@Patricks-MacBook-Pro.local>
2019-01-13 11:57:47 -0600

    Q260

---
+++ BitManipulation/260.Single_Number_III/260.Single_Number_III_BitManipulation.py
@@ -0,0 +1,17 @@
+class Solution(object):
+	def singleNumberIIIBitManipulation(self, nums):
+		"""
+		Time - O(n)
+		Space - O(1)
+		:type nums: List[int]
+		:rtype: int
+		"""
+
+
+	def singleNumberIIIBitManipulation2(self, nums):
+		"""
+		Time - O(n)
+		Space - O(1)
+		:type nums: List[int]
+		:rtype: int
+		"""
